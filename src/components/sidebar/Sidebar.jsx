a86d5a3c1ce3490cdfd964ba0f4d17deb1dba455
vikasrajput18 <vikasvikas98809@gmail.com>
2023-08-27 20:11:24 +0530

    seaech users

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { SidebarHeader } from "./header";
 import { Notifications } from "./notifications";
-import { Search } from "./search";
+import { Search, SearchResults } from "./search";
 import { Conversations } from "./conversation";
 
 const Sidebar = () => {
   const [searchResults, setSearchResults] = useState([]);
+
   return (
     <div className="w-[40%] h-full select-none">
       {/* sidebar header  */}
@@ -13,9 +14,18 @@ const Sidebar = () => {
       {/* notifications  */}
       <Notifications />
       {/* search  */}
-      <Search searchLength={searchResults?.length} />
+      <Search
+        searchLength={searchResults?.length}
+        setSearchResults={setSearchResults}
+      />
+      {searchResults.length > 0 ? (
+        <>
+          <SearchResults searchResults={searchResults} />
+        </>
+      ) : (
+        <Conversations />
+      )}
       {/* conversation  */}
-      <Conversations />
     </div>
   );
 };
